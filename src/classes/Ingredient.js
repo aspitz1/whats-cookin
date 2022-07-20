f34e32d100f737063b0f4eaf4389de5783298318
Anna Spitz <aspitzemail@gmail.com>
2022-07-19 20:18:29 -0500

    Deploy to gh pages

--- src/classes/Ingredient.js
+++
@@ -1,9 +0,0 @@
-class Ingredient {
-    constructor(ingredientDetails) {
-        this.id = ingredientDetails.id;
-        this.name = ingredientDetails.name;
-        this.estimatedCostInCents = ingredientDetails.estimatedCostInCents;
-    }
-}
-
-export default Ingredient;
